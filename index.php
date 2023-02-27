0eedb49a6faf4775be2fe49ac1c5aab207524150
Collins-Kareri <locoreri@gmail.com>
2023-02-27 15:37:16 +0300

    made contents for account settings style

--- index.php
+++ index.php
@@ -2,124 +2,114 @@
 include "./partials/head.php";
 ?>
 
-<?php
-include "./Calender.php";
-
-use Calender\Calender;
-use Carbon\Carbon;
-
-$current_month = Calender::buildMonth();
-$year = $current_month['year'];
-$month = $current_month['month'];
-$dates = $current_month['dates'];
-$days_initials = ['m', 't', 'w', 't', 'f', 's', 's'];
-
-/**
- * function to determine which corners on calender will be rounded
- */
-function round_corner($index)
-{
-    switch ($index) {
-        case 1:
-            return "rounded-tl-lg";
-        case 7:
-            return "rounded-tr-lg";
-        case 36:
-            return "rounded-bl-lg";
-        case 42:
-            return "rounded-br-lg";
-        default:
-            return "";
-    }
-}
-?>
-
-<div class="flex lg:flex-row flex-col w-full gap-10 before:content-[''] before:border before:border-slate-200 before:border-opacity-40 before:self-stretch">
-    <!-- calender container -->
-    <section class="flex-1 mb-6 lg:mb-0 lg:px-4 -order-1">
-        <div class="inline-flex items-center justify-between w-full mb-5 text-sm">
-            <h1 class="font-light"><?= "{$month} {$year}" ?></h1>
-            <div class="inline-flex h-fit items-center gap-2 text-slate-700 opacity-90">
-                <span class="bg-slate-800 px-3 py-1 rounded border border-slate-200 cursor-pointer">
-                    <i class="fa-solid fa-backward-step fa-lg"></i>
-                </span>
-                <span class="capitalize font-light mx-1 cursor-pointer">today</span>
-                <span class="bg-slate-800 px-3 py-1 rounded border border-slate-200 cursor-pointer">
-                    <i class="fa-solid fa-forward-step fa-lg"></i>
-                </span>
+<section class="w-full flex gap-8">
+    <!-- profile details container and inputs -->
+    <div class="flex-1 flex flex-col gap-4 bg-slate-900 rounded py-5 px-8 h-fit">
+        <h1 class="text-lg font-bold">Profile details</h1>
+        <!-- profile image container -->
+        <section class="inline-flex items-center gap-8">
+            <span class="inline-block w-[80px] h-[80px] rounded-full bg-slate-800"></span>
+            <div class="inline-flex gap-4 h-fit">
+                <button class="text-sm flex w-fit shrink-0 items-center px-4 py-3 bg-slate-800 ring-2 ring-slate-800 rounded gap-2 capitalize">
+                    change
+                </button>
+                <button class="text-sm flex w-fit shrink-0 items-center px-4 py-3 border-2 border-slate-200 rounded gap-2 capitalize">
+                    remove
+                </button>
             </div>
-        </div>
-
-        <div class="grid grid-cols-7">
-            <!-- loop through days in letters display that is monday to sunday -->
-            <?php foreach ($days_initials as $day_initial) : ?>
-                <!-- day letter container -->
-                <span class="inline-block text-center uppercase mb-1 font-extralight">
-                    <?= $day_initial ?>
-                </span>
-            <?php endforeach; ?>
-
-            <!-- index of the current date in the loop we start from 1 all the way to 42 as the array has 42 dates. Used to determine which corners to round -->
-            <?php $current_date_index = 1; ?>
-
-            <!-- loop through dates display in in the dates array-->
-            <?php foreach ($dates as $date_instance) : ?>
-                <!-- date container with different styling for dates not in the current month -->
-                <span class='text-sm inline-block text-center border px-3 py-2  border-slate-100  <?= $date_instance['within_month'] ? "cursor-pointer bg-slate-900" : "font-extralight cursor-not-allowed bg-slate-800" ?> <?= round_corner($current_date_index) ?>'>
-                    <p class=' w-9 h-9 mx-auto px-2 py-2 rounded-full <?= $date_instance['within_month'] ? "cursor-pointer hover:bg-cyan-50 hover:text-cyan-900" : "font-extralight cursor-not-allowed" ?> <?= $date_instance['is_today'] ? "bg-cyan-50 text-cyan-900 " : "" ?>'>
-                        <?= $date_instance['day_number'] ?>
-                    </p>
-                </span>
-                <!-- increase current date index by one. -->
-                <?php $current_date_index++; ?>
-            <?php endforeach; ?>
-        </div>
-    </section>
-
-    <!-- Tasks container -->
-    <section class="flex-1 lg:px-4">
-        <div class="inline-flex items-center justify-between flex-wrap w-full pb-6 mb-2">
-            <h1>Schedule for <?= $month ?> <?= Carbon::now()->format('d') ?>, <?= $year ?> </h1>
-            <button class=" text-sm flex w-fit shrink-0 items-center px-4 py-3 bg-slate-800 ring-2 ring-slate-800 rounded gap-2 capitalize">
-                add task
-            </button>
-        </div>
-
-
-        <section class="flex items-start flex-wrap w-full px-4 flex-row h-[450px] overflow-auto scroll_container">
-            <!--Single task container-->
-            <!-- <div class="flex w-fit flex-row py-4 border-opacity-30 px-4 h-fit bg-slate-800 rounded text-sm mb-4 shadow-inner shadow-slate-200">
-                Task details
-                <section class="w-full flex h-fit gap-x-4">
-                    <input type="checkbox" name="task_toggle" id="task_toggle" class="cursor-pointer rounded p-2 bg-slate-900 border-slate-900" />
-                    <section class="flex flex-col h-fit">
-                        <p class="font-extralight">7:00a.m - 8:00a.m</p>
-                        <p class="font-light">
-                            Lorem Ipsum is simply dummy text of the printing and
-                            typesetting industry.
-                        </p>
-                    </section>
-                </section>
-
-                Delete and edit task 
-                <section class="relative flex w-full justify-end gap-x-4 h-full">
-                    <span>
-                        <i class="fa-solid fa-pen-to-square fa-lg cursor-pointer h-fit opacity-80 hover:opacity-100"></i>
-                    </span>
-
-                    <span>
-                        <i class="fa-solid fa-trash fa-lg cursor-pointer opacity-80 hover:opacity-100"></i>
-                    </span>
-                </section>
-            </div> -->
-
-            <!--Placeholder content-->
-            <p class="w-full text-center font-light">
-                No tasks. You're all caught up.
-            </p>
         </section>
-    </section>
-</div>
+
+        <!-- profile details inputs -->
+        <section class="flex flex-wrap gap-4">
+            <!-- group inputs -->
+            <span class="inline-flex gap-4">
+                <!-- input container -->
+                <div class="flex-1">
+                    <label for="first_name" class="capitalize">first name</label>
+                    <div class="relative flex border border-slate-200 bg-slate-800 items-center rounded flex-wrap w-full">
+                        <input type="text" id="first_name" name="first_name" placeholder="first name" class="ring-0 border-0 outline-none bg-transparent w-full" />
+                    </div>
+                </div>
+                <!-- input container -->
+                <div class="flex-1">
+                    <label for="last_name" class="capitalize">last name</label>
+                    <div class="relative flex border border-slate-200 bg-slate-800 items-center rounded flex-wrap w-full">
+                        <input type="text" id="last_name" name="last_name" placeholder="last name" class="ring-0 border-0 outline-none bg-transparent w-full" />
+                    </div>
+                </div>
+            </span>
+
+            <span class="inline-flex gap-4 flex-col w-full">
+                <div class="flex-1">
+                    <label for="email" class="capitalize">email</label>
+                    <div class="relative flex border border-slate-200 bg-slate-800 items-center rounded flex-wrap w-full">
+                        <input type="email" id="email" name="email" placeholder="email" class="ring-0 border-0 outline-none bg-transparent w-full" />
+                    </div>
+                </div>
+                <!-- input container -->
+                <div class="flex-1">
+                    <label for="phone_number" class="capitalize">phone number</label>
+                    <div class="relative flex border border-slate-200 bg-slate-800 items-center rounded flex-wrap w-full">
+                        <span class="block relative px-2 border-r-2 font-extralight h-full border-slate-700">+254</span>
+                        <input type="tel" id="phone_number" name="phone_number" pattern="\([0-9]{3}\) [0-9]{2}-[0-9]{4}" placeholder="(712) 34-5678" class="ring-0 border-0 outline-none bg-transparent" />
+                    </div>
+                </div>
+            </span>
+
+        </section>
+
+        <!-- save changes to profile details -->
+        <section>
+            <button class="text-sm flex w-fit shrink-0 items-center px-4 py-3 bg-slate-800 ring-2 ring-slate-800 rounded gap-2 capitalize">
+                save
+            </button>
+        </section>
+    </div>
+
+    <!-- password change container and inputs -->
+    <div class="flex-1 flex flex-col gap-4 bg-slate-900 rounded py-5 px-8 h-fit">
+        <h1 class="text-lg font-bold">Security</h1>
+
+        <!-- password change inputs -->
+        <section class="flex flex-wrap gap-4">
+            <span class="inline-flex gap-4 flex-col w-full">
+                <!-- input container -->
+                <div class="w-full">
+                    <label for="current_password" class="capitalize">current password</label>
+                    <div class="relative flex border border-slate-200 bg-slate-800 items-center rounded flex-wrap w-full">
+                        <input type="password" id="current_password" name="current_password" placeholder="current password" class="ring-0 border-0 outline-none bg-transparent w-full" />
+                    </div>
+                    <span class="cursor-pointer underline underline-offset-4 text-sm">Forgot password?</span>
+                </div>
+
+                <!-- input container -->
+                <div class="w-full">
+                    <label for="new_password" class="capitalize">new password</label>
+                    <div class="relative flex border border-slate-200 bg-slate-800 items-center rounded flex-wrap w-full">
+                        <input type="password" id="new_password" name="new_password" placeholder="new password" class="ring-0 border-0 outline-none bg-transparent w-full" />
+                    </div>
+                </div>
+
+                <!-- input container -->
+                <div class="w-full">
+                    <label for="confirm_new_password" class="capitalize">confirm new password</label>
+                    <div class="relative flex border border-slate-200 bg-slate-800 items-center rounded flex-wrap w-full">
+                        <input type="password" id="confirm_new_password" name="confirm_new_password" placeholder="confirm new password" class="ring-0 border-0 outline-none bg-transparent w-full" />
+                    </div>
+                </div>
+            </span>
+
+        </section>
+
+        <!-- save changes to profile details -->
+        <section>
+            <button class="text-sm flex w-fit shrink-0 items-center px-4 py-3 bg-slate-800 ring-2 ring-slate-800 rounded gap-2 capitalize">
+                change
+            </button>
+        </section>
+    </div>
+</section>
+
 
 <?php
 include "./partials/footer.php"
