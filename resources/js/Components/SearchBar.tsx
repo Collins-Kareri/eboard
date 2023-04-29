4ac7acd209408fa6fef4511f4d6b9ac64be2dc3d
Collins-Kareri <locoreri@gmail.com>
2023-04-29 06:56:10 +0300

    Chore: color change

--- resources/js/Components/SearchBar.tsx
+++ resources/js/Components/SearchBar.tsx
@@ -3,7 +3,7 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 function SearchBar() {
     return (
-        <div className="tw-border tw-border-slate-800 tw-flex tw-items-center tw-rounded-lg tw-border-solid tw-mt-2 tw-bg-slate-50 tw-w-11/12">
+        <div className="tw-border tw-border-slate-800 tw-flex tw-items-center tw-rounded-lg tw-border-solid tw-mt-2 tw-bg-slate-950 tw-w-11/12">
             <FontAwesomeIcon
                 icon={faMagnifyingGlass}
                 className="tw-border-none tw-p-2"
