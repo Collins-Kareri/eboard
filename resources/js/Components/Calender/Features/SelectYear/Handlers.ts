c1f5bbf099785768739bf236a6d365f1ae7369cb
Collins-Kareri <locoreri@gmail.com>
2023-05-01 22:54:17 +0300

    Fix: remove limit on start year of current year.

--- resources/js/Components/Calender/Features/SelectYear/Handlers.ts
+++ resources/js/Components/Calender/Features/SelectYear/Handlers.ts
@@ -3,12 +3,12 @@ import generateYears from "@/Utils/generateYears";
 
 export const handleScroll = (
     evt: React.UIEvent<HTMLUListElement, UIEvent>,
-    props: SelectYearProps
+    props: Omit<SelectYearProps, "setCurrentYear">
 ) => {
     const container = evt.target as HTMLUListElement,
         { scrollTop, clientHeight, scrollHeight } = container;
 
-    if (scrollTop && props.yearsRange[0] > props.currentYear) {
+    if (scrollTop === 0) {
         props.setYearsRange(generateYears(props.yearsRange[0] - 10));
     }
 
