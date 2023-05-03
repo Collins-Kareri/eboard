1dae5f385a5b576434e8b2681d44fe965b1fc3e3
Collins-Kareri <locoreri@gmail.com>
2023-05-03 06:54:02 +0300

    Fix: Add xl size

--- resources/js/Components/Avatar.tsx
+++ resources/js/Components/Avatar.tsx
@@ -1,6 +1,6 @@
 export interface AvatarComponentProps {
     avatarUrl: string;
-    size: "sm" | "md" | "lg";
+    size: "sm" | "md" | "lg" | "xl";
 }
 
 function Avatar({
@@ -8,16 +8,19 @@ function Avatar({
     size,
 }: {
     avatarUrl: string;
-    size: "sm" | "md" | "lg";
+    size: "sm" | "md" | "lg" | "xl";
 }) {
-    const containerSize: { sm: string; md: string; lg: string } = {
+    const containerSize: { sm: string; md: string; lg: string; xl: string } = {
         sm: "tw-w-10 tw-h-10",
         md: "tw-w-12 tw-h-12",
         lg: "tw-w-14 tw-h-14",
+        xl: "tw-w-20 tw-h-20",
     };
 
     return (
-        <span className={`tw-block ${containerSize[size]} tw-cursor-pointer`}>
+        <span
+            className={`tw-block ${containerSize[size]} tw-cursor-pointer tw-w-`}
+        >
             <img
                 className="tw-relative tw-w-full tw-h-full tw-rounded-full"
                 src={`${avatarUrl}`}
