11527600b133d9c8db5de25634d8f1c6591d3760
Collins-Kareri <locoreri@gmail.com>
2023-05-05 22:14:00 +0300

    Feature: generate test super user.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Departments;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,7 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->for(Departments::factory()->state([
+            'name'=>'hr'
+        ]), 'memberOf')
+        ->create([
+            'first_name'=>'John',
+            'last_name'=>'Doe',
+            'email'=>'johnDoe@mail.com',
+            'job_title'=>'hr manager',
+            'password'=>Hash::make('secret'),
+            'owns_department'=>true
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
