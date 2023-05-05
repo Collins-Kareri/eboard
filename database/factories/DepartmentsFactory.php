6444fcb5f1ea72bc91d03398febcb68446793d99
Collins-Kareri <locoreri@gmail.com>
2023-05-05 22:13:11 +0300

    Feature: generate dummy data for departments table

--- database/factories/DepartmentsFactory.php
+++ database/factories/DepartmentsFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Arr;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Departments>
@@ -17,11 +16,8 @@ class DepartmentsFactory extends Factory
      */
     public function definition(): array
     {
-        $user_ids = range(1, 10);
         return [
-            //
-            'name' => fake()->company(),
-            'owner' => Arr::random($user_ids)
+            'name' => fake()->company()
         ];
     }
 }
