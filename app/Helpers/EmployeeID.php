c7bd1258c68711ffdc39e2471aab54f44295a415
Collins-Kareri <locoreri@gmail.com>
2023-05-05 22:10:45 +0300

    Feature: generate employee id

--- app/Helpers/EmployeeID.php
+++ app/Helpers/EmployeeID.php
@@ -4,11 +4,19 @@ namespace App\Helpers;
 
 use App\Models\User;
 
-class EmployeeId
+class EmployeeID
 {
+    /**
+     * Generate employee id of the form EB-x, where x is based on the count of users in the users table.
+     */
     public static function generate()
     {
-        $userCount=User::count();
-        return "EB-".($userCount+1);
+        $start=User::count();
+        return "EB-".($start+1);
+    }
+
+    public static function factoryGenerate(int $start)
+    {
+        return "EB-".($start+1);
     }
 }
