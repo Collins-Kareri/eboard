10d743e4e7f3d0c0a10c3ce907e2e060459c1963
Collins-Kareri <locoreri@gmail.com>
2023-05-08 15:30:41 +0300

    Feature: Toggle password visibility.

--- resources/js/Pages/Profile/partials/Update.Password.tsx
+++ resources/js/Pages/Profile/partials/Update.Password.tsx
@@ -1,7 +1,13 @@
 import FormInputsLayout from "@/Layouts/FormInputs.Layout";
 import ProfileSectionLayout from "@/Layouts/ProfileSection.Layout";
+import { useState } from "react";
+import togglePasswordVisibility from "@/Utils/togglePasswordVisibility";
 
 function UpdatePassword() {
+    const [passwordVisibility, setPasswordVisibility] = useState<
+        "show" | "hide"
+    >("hide");
+
     return (
         <ProfileSectionLayout
             title={"Security"}
@@ -13,31 +19,53 @@ function UpdatePassword() {
                     htmlFor="current_password"
                 >
                     <input
-                        type="password"
+                        type={
+                            passwordVisibility === "show" ? "text" : "password"
+                        }
                         id="current_password"
                         autoComplete="current-password"
                     />
                 </FormInputsLayout>
+
                 <FormInputsLayout
                     labelText={"new password"}
                     htmlFor="new_password"
                 >
                     <input
-                        type="password"
+                        type={
+                            passwordVisibility === "show" ? "text" : "password"
+                        }
                         id="new_password"
                         autoComplete="new-password"
                     />
                 </FormInputsLayout>
+
                 <FormInputsLayout
                     labelText={"confirm new password"}
                     htmlFor="confirm_new_password"
                 >
                     <input
-                        type="password"
+                        type={
+                            passwordVisibility === "show" ? "text" : "password"
+                        }
                         id="confirm_new_password"
                         autoComplete="new-password"
                     />
                 </FormInputsLayout>
+
+                <span className="tw-flex tw-items-center tw-w-fit tw-gap-2">
+                    <input
+                        type="checkbox"
+                        className="tw-p-2"
+                        onChange={() =>
+                            togglePasswordVisibility(
+                                passwordVisibility,
+                                setPasswordVisibility
+                            )
+                        }
+                    />
+                    <p>Show password.</p>
+                </span>
             </div>
 
             <div className="tw-w-full tw-border-t tw-border-slate-50 tw-flex tw-justify-end tw-py-4">
