007f3750b14df603392ef884b247b71abbd11bd2
Collins-Kareri <locoreri@gmail.com>
2023-05-08 15:23:45 +0300

    Fix: Add default value for avatar_url.

--- resources/js/Components/Avatar.tsx
+++ resources/js/Components/Avatar.tsx
@@ -1,23 +1,23 @@
+import { PageProps } from "@/types";
+import { usePage } from "@inertiajs/react";
+
 export interface AvatarComponentProps {
-    avatarUrl?: string;
+    avatar_url?: string;
     size: "sm" | "md" | "lg" | "xl";
 }
 
-function Avatar({
-    avatarUrl = "https://ui-avatars.com/api/?name=jd&color=#060406&background=#DFF3E4",
-    size,
-}: AvatarComponentProps) {
+function Avatar({ avatar_url, size }: AvatarComponentProps) {
     const containerSize: { sm: string; md: string; lg: string; xl: string } = {
-        sm: "tw-w-10 tw-h-10",
-        md: "tw-w-12 tw-h-12",
-        lg: "tw-w-14 tw-h-14",
-        xl: "tw-w-20 tw-h-20",
-    };
+            sm: "tw-w-10 tw-h-10",
+            md: "tw-w-12 tw-h-12",
+            lg: "tw-w-14 tw-h-14",
+            xl: "tw-w-20 tw-h-20",
+        },
+        avatarUrl =
+            avatar_url ?? usePage<PageProps>().props.auth.user.avatar_url;
 
     return (
-        <span
-            className={`tw-block ${containerSize[size]} tw-cursor-pointer tw-w-`}
-        >
+        <span className={`tw-block ${containerSize[size]} tw-cursor-pointer`}>
             <img
                 className="tw-relative tw-w-full tw-h-full tw-rounded-full"
                 src={`${avatarUrl}`}
