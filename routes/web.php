4b577cb5a86cc02e25f82070cfd60ad301571320
Collins-Kareri <locoreri@gmail.com>
2023-05-08 15:47:05 +0300

    Refactor: Remove commented code.

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-// use App\Http\Controllers\ProfileController;
-// use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Helpers\Calender;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -55,11 +55,18 @@ Route::middleware('auth')->group(function () {
         return Inertia::render('Employees');
     })->name('employees');
 
-    Route::get('/profile', function () {
-        return Inertia::render('Profile/Profile');
-    })->name('profile');
+    Route::controller(ProfileController::class)->group(function () {
+        Route::get('/profile', 'edit')->name('profile.edit');
+        Route::patch('/profile', 'update')->name('profile.update');
+        Route::delete('/profile', 'destroy')->name('profile.destroy');
+    });
 
-    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+    Route::delete('/avatar', function (Request $request) {
+        $request->user()->deleteAvatar();
+        return redirect()->route('profile.update');
+    })->name('avatar.destory');
+
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 });
 
 // Route::get('/dashboard', function () {
