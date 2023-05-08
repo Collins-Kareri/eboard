ab0f077c3ada1d4db059e2aefaf0d1c25a4a0839
Collins-Kareri <locoreri@gmail.com>
2023-05-08 15:23:05 +0300

    Fix: Add default value for owns_department column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ return new class () extends Migration {
             $table->string('phone_number')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('owns_department');
+            $table->boolean('owns_department')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
