ed1d5e82698e21635fc3d01f0106afdc88b83db2
Collins-Kareri <locoreri@gmail.com>
2023-05-08 15:21:39 +0300

    Feature: Logic to delete and update user avatar.

--- app/Traits/HasAvatar.php
+++ app/Traits/HasAvatar.php
@@ -2,8 +2,12 @@
 
 namespace App\Traits;
 
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Illuminate\Http\UploadedFile;
 
 /**
  * USER HAS AVATAR
@@ -12,14 +16,48 @@ trait HasAvatar
 {
     //todo updateAvatar,deleteAvatar,getAvatar,defaultUrl
     /**
+     * Generate avatar name
+     */
+    private function generateName()
+    {
+        $names=Str::of($this->full_name)->explode(" ");
+        $initials="";
+        $randomUlid=Str::ulid(Carbon::now());
+
+        for($index=0;$index<count($names);$index++) {
+            $initials=$initials.$names[$index][0];
+        }
+
+        return Str::lower("{$initials}-eboard-{$randomUlid}");
+    }
+
+    /**
      * Upload avatar
      */
-    public function updateAvatar()
+    public function updateAvatar(UploadedFile $file)
     {
-        // $avatarData = file_get_contents(fake()->imageUrl());
-        // $avatarName = "avatars/" . fake()->uuid() . '.png';
-        // Storage::put($avatarName, $avatarData, 'public');
-        // $avatar = Storage::getCLient()->getObjectUrl('eboard', $avatarName);
+        $filePath= $this->avatar ?? $this->generateName().'.'.$file->extension();
+
+        // store in minio
+        $imageName=$file->storePubliclyAs('', $filePath);
+
+        $this->update([
+            'avatar'=>$imageName
+        ]);
+    }
+
+     /**
+     * Delete avatar
+     */
+    public function deleteAvatar()
+    {
+        $deleteRes=Storage::delete($this->avatar);
+
+        if($deleteRes) {
+            $this->update([
+                        'avatar'=>null
+                    ]);
+        }
     }
 
     /**
@@ -29,7 +67,7 @@ trait HasAvatar
     {
         return Attribute::get(
             function () {
-                return $this->avatar ? Storage::getCLient()->getObjectUrl('eboard', $this->avatar) : $this->defaultUrl();
+                return $this->avatar ? env('AWS_URL').'/'.env('AWS_BUCKET').'/'.$this->avatar : $this->defaultUrl();
             }
         );
     }
@@ -39,7 +77,7 @@ trait HasAvatar
      */
     private function defaultUrl()
     {
-        $name = urlencode($this->first_name . " " . $this->last_name);
+        $name = urlencode($this->full_name);
         return "https://ui-avatars.com/api/?name={$name}&color=#060406&background=#DFF3E4";
     }
 }
