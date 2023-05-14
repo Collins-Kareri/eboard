31d78b7b7f0155a3da0797eb38f53cd08a251bf3
Collins-Kareri <locoreri@gmail.com>
2023-05-14 08:15:48 +0300

    Add department link

--- resources/js/Components/Nav/Features/Profile.Nav.tsx
+++ resources/js/Components/Nav/Features/Profile.Nav.tsx
@@ -7,7 +7,7 @@ import { Link, usePage } from "@inertiajs/react";
 
 function Profile() {
     const { url, props } = usePage<PageProps>(),
-        { full_name, current_department, owns_department } = props.auth.user;
+        { full_name, current_department, role } = props.auth.user;
 
     return (
         <>
@@ -41,10 +41,10 @@ function Profile() {
                         >
                             <Popover.Panel
                                 className={
-                                    "tw-absolute tw-top-[73px] tw-left-0 md:tw-left-1/2 tw-w-full md:tw-w-1/2 tw-flex tw-justify-end tw-bg-slate-200 tw-p-4 tw-shadow-md tw-shadow-slate-100 tw-flex-col tw-gap-2 md:tw-rounded-l-md tw-z-10"
+                                    "tw-absolute tw-top-[81px] tw-left-0 md:tw-left-1/2 tw-w-full md:tw-w-1/2 tw-flex tw-justify-end tw-bg-slate-200 tw-p-4 tw-shadow-md tw-shadow-slate-100 tw-flex-col tw-gap-2 md:tw-rounded-l-md tw-z-10"
                                 }
                             >
-                                <div className="tw-border-b tw-border-slate-100 tw-rounded-md tw-w-full tw-text-right tw-flex tw-h-fit tw-flex-col tw-pb-2 tw-gap-1">
+                                <div className="tw-border-b tw-border-slate-100 tw-rounded-md tw-w-full tw-text-right tw-flex tw-h-fit tw-flex-col tw-pb-3 tw-gap-3">
                                     <h6 className="tw-text-xs tw-opacity-50">
                                         Manage settings
                                     </h6>
@@ -57,6 +57,16 @@ function Profile() {
                                         }`}
                                     >
                                         Profile
+                                    </Link>
+                                    <Link
+                                        href={route("department")}
+                                        className={`tw-p-2 hover:tw-bg-slate-400 tw-rounded-md ${
+                                            url === "/department"
+                                                ? "tw-bg-slate-400"
+                                                : ""
+                                        }`}
+                                    >
+                                        Department
                                     </Link>
                                 </div>
                                 <Link
@@ -71,8 +81,7 @@ function Profile() {
                                 <span className="tw-block tw-w-full tw-text-right tw-text-xs tw-font-thin tw-italic tw-text-slate-100 tw-opacity-50 tw-capitalize">
                                     <p>{full_name}.</p>
                                     <p>
-                                        {owns_department ? "manager, " : ""}
-                                        {current_department}.
+                                        {role}, {current_department}.
                                     </p>
                                 </span>
                             </Popover.Panel>
