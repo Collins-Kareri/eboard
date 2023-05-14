f815bd3b9f9bee0e0384d3c1c164b71b465c3b43
Collins-Kareri <locoreri@gmail.com>
2023-05-14 13:43:49 +0300

    Fix: change to use named routes

--- resources/js/Components/Nav/NavLink.tsx
+++ resources/js/Components/Nav/NavLink.tsx
@@ -1,14 +1,18 @@
 import { Link, usePage } from "@inertiajs/react";
+import getUrl from "@/Utils/getUrl";
+import { LinkProps } from "@/Components/Nav/Links";
 
-function NavLink({ to, textContent }: { to: string; textContent: string }) {
-    const { url } = usePage();
+function NavLink({ to, textContent }: LinkProps) {
+    const { url } = usePage(),
+        currentUrl = getUrl(route(to)),
+        active = currentUrl === url;
 
     return (
         <Link
             className={`tw-capitalize tw-leading-5 tw-pt-2 tw-px-1 ${
-                url === to ? "tw-border-b-2" : ""
+                active ? "tw-border-b-2" : ""
             } tw-border-slate-400 hover:tw-border-b-2`}
-            href={to}
+            href={route(to)}
         >
             {textContent}
         </Link>
