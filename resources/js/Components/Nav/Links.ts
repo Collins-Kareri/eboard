ce3931d0671bcc2e8046fb9e1e262657779bd969
Collins-Kareri <locoreri@gmail.com>
2023-05-14 13:42:58 +0300

    Fix: add dropdown routes

--- resources/js/Components/Nav/Links.ts
+++ resources/js/Components/Nav/Links.ts
@@ -1,21 +1,38 @@
-interface LinkProps {
+export interface LinkProps {
     to: string;
     textContent: string;
 }
 
-const myLinks: LinkProps[] = [
-    {
-        to: "/",
-        textContent: "home",
-    },
-    {
-        to: "/tasks",
-        textContent: "tasks",
-    },
-    {
-        to: "/employees",
-        textContent: "employees",
-    },
-];
+interface Links {
+    nav: LinkProps[];
+    dropDownNav: LinkProps[];
+}
+
+const myLinks: Links = {
+    nav: [
+        {
+            to: "home",
+            textContent: "home",
+        },
+        {
+            to: "tasks",
+            textContent: "tasks",
+        },
+        {
+            to: "employees",
+            textContent: "employees",
+        },
+    ],
+    dropDownNav: [
+        {
+            to: "profile.edit",
+            textContent: "profile",
+        },
+        {
+            to: "department",
+            textContent: "departments",
+        },
+    ],
+};
 
 export default myLinks;
