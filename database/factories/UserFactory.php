5ef7ac226fd507416c39702d6cef252736e921b9
Collins-Kareri <locoreri@gmail.com>
2023-05-14 08:13:28 +0300

    Make default password to be secret.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,9 +2,11 @@
 
 namespace Database\Factories;
 
+use App\Enums\UserRole;
 use App\Helpers\EmployeeID;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -27,8 +29,9 @@ class UserFactory extends Factory
             'phone_number' => fake()->e164PhoneNumber(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('secret'), // password
             'remember_token' => Str::random(10),
+            'role'=>UserRole::Member->value
         ];
     }
 
