89d6b9992ae268e64c2126ac3d3d15e61f596138
Collins-Kareri <locoreri@gmail.com>
2023-05-15 14:35:03 +0300

    Refactor: Remove unused code

--- app/Http/Controllers/DepartmentInvitationController.php
+++ app/Http/Controllers/DepartmentInvitationController.php
@@ -10,7 +10,6 @@ use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
-use Illuminate\Validation\Rules\Enum;
 
 class DepartmentInvitationController extends Controller
 {
