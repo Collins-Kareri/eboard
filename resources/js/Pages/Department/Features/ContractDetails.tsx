2846fe20b58059a187ac13a9fef44b3d49604401
Collins-Kareri <locoreri@gmail.com>
2023-05-15 14:40:22 +0300

    Feature: Ask for contract details from user.

---
+++ resources/js/Pages/Department/Features/ContractDetails.tsx
@@ -0,0 +1,104 @@
+import FormInputsLayout from "@/Layouts/FormInputs.Layout";
+import handleTyping, { DataType, ErrorType } from "@/Handlers/handleTyping";
+import { useRef, useState } from "react";
+import { Listbox } from "@headlessui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import toPlural from "@/Utils/toPlural";
+
+export default function ContractDetails<clearErrorsType>({
+    data,
+    errors,
+    clearErrors,
+    setData,
+}: {
+    data: DataType;
+    errors: ErrorType;
+    clearErrors: (...fields: clearErrorsType[]) => void;
+    setData: (id: keyof DataType, value: string) => void;
+}) {
+    const timeMeasures: string[] = ["day", "week", "month", "year"],
+        [timeMeasure, setTimeMeasure] = useState(timeMeasures[0]),
+        contractPeriodInputRef = useRef(null);
+
+    function changeTimeMeasure(e: React.ChangeEvent<HTMLInputElement>) {
+        const value = e.target.value;
+        setData("contract_period", `${value} ${timeMeasure}`);
+    }
+
+    return (
+        <div className="tw-flex tw-flex-col tw-gap-6">
+            <FormInputsLayout
+                labelText={"start time"}
+                htmlFor="start_time"
+                errors={errors.start_time}
+            >
+                <input
+                    type="date"
+                    id="start_time"
+                    value={
+                        data.start_time as "string | number | readonly string[] | undefined"
+                    }
+                    onChange={(e) =>
+                        handleTyping(e, data, errors, clearErrors, setData)
+                    }
+                />
+            </FormInputsLayout>
+
+            <FormInputsLayout
+                labelText={"contract period"}
+                htmlFor="contract_period"
+                errors={errors.contract_period}
+            >
+                <section className="tw-flex tw-bg-slate-950 tw-border tw-border-slate-900 tw-rounded-md">
+                    <input
+                        type="number"
+                        id="contract_period"
+                        onChange={changeTimeMeasure}
+                        ref={contractPeriodInputRef}
+                        className="tw-flex-1 !tw-bg-transparent tw-border-none focus:tw-ring-0"
+                    />
+                    <Listbox value={timeMeasure} onChange={setTimeMeasure}>
+                        <Listbox.Button
+                            className={
+                                "tw-border-none tw-flex tw-items-center tw-gap-2 hover:tw-bg-slate-700"
+                            }
+                        >
+                            <span>
+                                {contractPeriodInputRef &&
+                                contractPeriodInputRef.current
+                                    ? toPlural(
+                                          timeMeasure,
+                                          (
+                                              contractPeriodInputRef.current as unknown as HTMLInputElement
+                                          ).value as unknown as number
+                                      )
+                                    : timeMeasure}
+                            </span>
+                            <FontAwesomeIcon icon={faAngleDown} />
+                        </Listbox.Button>
+                        <Listbox.Options
+                            className={
+                                "tw-absolute tw-bg-slate-900 tw-top-[70px] tw-right-0 tw-w-full tw-rounded tw-flex tw-flex-col tw-gap-2 tw-z-10 tw-p-3 tw-px-4 tw-capitalize"
+                            }
+                        >
+                            {timeMeasures.map((measure, index) => {
+                                return (
+                                    <Listbox.Option
+                                        key={index}
+                                        value={measure}
+                                        className={
+                                            "hover:tw-bg-slate-400 tw-py-2 tw-px-2 tw-cursor-pointer tw-rounded-md"
+                                        }
+                                    >
+                                        {measure}
+                                    </Listbox.Option>
+                                );
+                            })}
+                        </Listbox.Options>
+                    </Listbox>
+                </section>
+            </FormInputsLayout>
+        </div>
+    );
+}
