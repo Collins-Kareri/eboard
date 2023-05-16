b830532fd2f23efde652a79dc66dabaab09a8e71
Collins-Kareri <locoreri@gmail.com>
2023-05-16 11:06:06 +0300

    Test: login feature.

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\Departments;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+
+beforeEach(function () {
+    $this->departments=Departments::factory()->state([
+        'name'=>'hr'
+    ])->create();
+    $this->user=User::factory()->state([
+        'email'=>'dev@eboard.com'
+    ])->for($this->departments)->create();
+});
+
+it('redirects to login if unauthenticated', function () {
+    $response=$this->get(RouteServiceProvider::HOME);
+
+    $response->assertRedirect(route('login.create'));
+});
+
+it("Fails login with wrong credentials", function (string $email, string $password) {
+    $response=$this->post(route('login.store'), [
+        'email'=>$email,
+        'password'=>$password
+    ]);
+
+    $response->assertInvalid();
+})->with([
+    'wrong email'=>['nonexistant@mail.com','s£cRet123'],
+    'invalid password'=>['dev@eboard.com','wrong_password']
+]);
+
+
+it('Logs in user', function () {
+    $response=$this->post(route('login.store'), [
+        'email'=>$this->user->email,
+        'password'=>'s£cReT123'
+    ]);
+
+    $this->assertAuthenticated();
+    $response->assertRedirect(RouteServiceProvider::HOME);
+});
