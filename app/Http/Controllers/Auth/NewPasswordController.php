7b7c517a248cddc95ee9ba2d35c933f00cc64540
Collins-Kareri <locoreri@gmail.com>
2023-05-16 17:53:25 +0300

    Add validation rules for password field

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -37,7 +37,7 @@ class NewPasswordController extends Controller
         $request->validate([
             'token' => 'required',
             'email' => 'required|email',
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', 'confirmed', Rules\Password::min(8)->mixedCase()->numbers()->symbols()->letters()],
         ]);
 
         // Here we will attempt to reset the user's password. If it is successful we
@@ -59,7 +59,7 @@ class NewPasswordController extends Controller
         // the application's home authenticated view. If there is an error we can
         // redirect them back to where they came from with their error message.
         if ($status == Password::PASSWORD_RESET) {
-            return redirect()->route('login')->with('status', __($status));
+            return redirect()->route('login.create')->with('status', __($status));
         }
 
         throw ValidationException::withMessages([
