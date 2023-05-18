f5b22798486e98db77297b26ac4eee583667c386
Collins-Kareri <locoreri@gmail.com>
2023-05-18 12:56:11 +0300

    Chore: Added documentation for args.

--- app/Traits/UserInvite.php
+++ app/Traits/UserInvite.php
@@ -14,6 +14,10 @@ trait UserInvite
 {
     /**
      * send invite to user
+     * @param string $email the email of the invited user
+     * @param string $role the role of the invited user
+     * @param string $department the department the invited user will be a part of
+     * @param string $route the route that will be in the signed url generated for the invitee. If a department creation we use onboard.create else register.create
      */
     public function sendInvite(string $email, string $role, string $department, string $route)
     {
