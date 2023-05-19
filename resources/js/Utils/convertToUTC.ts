1b67016dbef65fa9dc45b5c97bbac2329b49258b
Collins-Kareri <locoreri@gmail.com>
2023-05-19 13:58:48 +0300

    Fix: get utc date instead of timestamp

--- resources/js/Utils/convertToUTC.ts
+++ resources/js/Utils/convertToUTC.ts
@@ -1,11 +1,4 @@
 export default function convertToUTC(date: Date) {
     const userDate = new Date(date);
-    return Date.UTC(
-        userDate.getFullYear(),
-        userDate.getMonth(),
-        userDate.getDay(),
-        userDate.getHours(),
-        userDate.getSeconds(),
-        userDate.getMilliseconds()
-    );
+    return userDate.toUTCString();
 }
