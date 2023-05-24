705ae1e666a48ec42af99686097d4abb48055b8e
Collins-Kareri <locoreri@gmail.com>
2023-05-24 15:17:48 +0300

    Fix: Use new userpolicy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use App\Models\User;
-use App\Policies\DepartmentInvitePolicy;
+use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -15,7 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         //
-        User::class=>DepartmentInvitePolicy::class
+        User::class=>UserPolicy::class
     ];
 
     /**
