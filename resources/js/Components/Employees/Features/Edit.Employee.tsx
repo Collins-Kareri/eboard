4ec7c349479181f3f7839c970ce1bb647dadbbe4
Collins-Kareri <locoreri@gmail.com>
2023-05-24 15:20:32 +0300

    Refactor: Remove unused logic

--- resources/js/Components/Employees/Features/Edit.Employee.tsx
+++ resources/js/Components/Employees/Features/Edit.Employee.tsx
@@ -1,17 +1,4 @@
-import { Fragment, useState } from "react";
-import Icon from "@/Components/Icon";
-import {
-    faAngleLeft,
-    faPencilAlt,
-    faXmark,
-} from "@fortawesome/free-solid-svg-icons";
-import { EmployeesProps } from "@/Components/Employees/EmployeesList";
-import Avatar from "@/Components/Avatar";
-import { Transition, Dialog } from "@headlessui/react";
-
-function EditEmployees({ employee }: { employee: EmployeesProps }) {
-    const [data, setData] = useState();
-
+function EditEmployees() {
     return (
         <section className="tw-flex tw-flex-col tw-gap-6">
             to be implemented
